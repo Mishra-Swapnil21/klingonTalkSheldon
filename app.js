014d04d34429a6f72eee87f061e6455e4837defd
Mishra swapnil <sammishra644@gmail.com>
2022-01-24 13:07:22 +0530

    updated css

--- app.js
+++ app.js
@@ -2,31 +2,35 @@ console.log("script is working")
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#input");
 var txtOutput = document.querySelector("#output");
+
 var serverURL = "https://api.funtranslations.com/translate/klingon.json"
+
+
 
 
 function getTranslationURL(input){
     return serverURL + "?" + "text=" + input
-};
+}
 
-function errorHandler() {
+function errorHandler(error) {
     console.log("error occured" , error);
     alert("something went wrong with server! try again after some time")
-};
+}
 
 function clickHandler() {
-    txtOutput.innerText = txtInput.value; 
-    var inputText = txtInput.value;//taking input
+     var inputText = txtInput.value;  //taking input
 
     //calling server for processing
     fetch(getTranslationURL(inputText))
-       .then(response => response.json())
+       .then(respose => respose.json())
        .then(json => {
-           var translatedText = json.contents.translated;
-           txtOutput.innerText = translatedText ; // output
-       })
+         var translatedText = json.contents.translated;
+         txtOutput.innerText = translatedText;
+       })  
+       .catch(errorHandler)
        
-};
+};     
+
 
 
 btnTranslate.addEventListener("click", clickHandler)
